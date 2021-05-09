3b1db9d2031e917a07cc697b4b79b9d2cf0239d6
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-09 14:08:17 +0600

    added activityB, which includes fragmentB

--- app/src/main/java/kg/tutorialapp/hw_36/ActivityB.kt
+++ app/src/main/java/kg/tutorialapp/hw_36/ActivityB.kt
@@ -7,5 +7,13 @@ class ActivityB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
+        setup()
+    }
+
+    private fun setup() {
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container_B, FragmentB(), FragmentB.TAG)
+            .commit()
     }
 }
