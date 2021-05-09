6022096504a81ff82ace7c4eb36124ace1b828e7
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-09 14:25:59 +0600

    added activityC, which includes fragmentC

--- app/src/main/java/kg/tutorialapp/hw_36/ActivityC.kt
+++ app/src/main/java/kg/tutorialapp/hw_36/ActivityC.kt
@@ -7,5 +7,14 @@ class ActivityC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c)
+        setup()
+    }
+
+    private fun setup() {
+        val text = getString(R.string.blank_fragment_C)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container_C, FragmentC.newInstance(text), FragmentC.TAG)
+            .commit()
     }
 }
